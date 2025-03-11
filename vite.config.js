4ea558c71f04547d5bd3a7a2dfadbdeb70698464
amonandre12 <amonblinwadjaandre@gmail.com>
2025-03-11 11:19:00 +0000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/AMON/',
+  base:'/PORT/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',import.meta.url))
     },
   },
 })
